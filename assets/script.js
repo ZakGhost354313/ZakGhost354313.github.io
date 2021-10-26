a92a83a834a43b8ff64f86c747389638f8d39b54
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2021-10-26 17:11:26 -0500

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+function displayMenu(currentMenu) {
+				var thisMenu = document.getElementById(currentMenu).style
+				if (thisMenu.display == "block") {
+					thisMenu.display = "none"
+				}
+				else {
+					thisMenu.display = "block"
+				}
+				return false;
+			}
+			function cookieAdd(name, value) {
+				document.cookie = name+"="+value;
+				return true;
+			}
+      let cloudflareCookie1 = cookieAdd("cloudflare-test","allowed");
