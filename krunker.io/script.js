8cd51b5e7543c0797b445bf64c5c6a94a1a35f64
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2021-11-11 14:57:05 -0600

    Update script.js

--- krunker.io/script.js
+++ krunker.io/script.js
@@ -6,4 +6,4 @@
 var html = "<iframe src='https://krunker.io' style='border:none;' title='Krunker.io Random Game' width='1080' height='720'></iframe>"
 var taget = document.getElementById("App");
 target.innerHTML += html;
-/*
+*/
